7f9de1f3b6913c612e0182520539d2de946be4f3
TheElizarov <elizarov1988@gmail.com>
2025-03-17 20:43:21 +0400

    Task AddToNumbers.kt: Решение задачи - добавлена ссылка на пост с решением

--- src/main/kotlin/leetcode/AddToNumbers.kt
+++ src/main/kotlin/leetcode/AddToNumbers.kt
@@ -1,6 +1,8 @@
 package ru.theelizarov.leetcode
 
 /**
+ * Solution https://leetcode.com/problems/add-two-numbers/solutions/6548215/solve-add-two-numbers-kotlin-by-theeliza-lbqh/
+ *
  * Task https://leetcode.com/problems/add-two-numbers/description/
  *
  * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
