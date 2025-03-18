f039b469243acec7718da0041eedaa07dcc9a39f
TheElizarov <elizarov1988@gmail.com>
2025-03-18 13:03:34 +0400

    Task SortingMarkToysSolution.kt: Добавлен файл с описанием новой задачи

---
+++ src/main/kotlin/hackerrank/SortingMarkToysSolution.kt
@@ -0,0 +1,44 @@
+package ru.theelizarov.hackerrank
+
+/**
+ * Task https://www.hackerrank.com/challenges/mark-and-toys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+ *
+ * Mark and Jane are very happy after having their first child. Their son loves toys, so Mark wants to buy some. There are a number of different toys lying in front of him, tagged with their prices. Mark has only a certain amount to spend, and he wants to maximize the number of toys he buys with this money. Given a list of toy prices and an amount to spend, determine the maximum number of gifts he can buy.
+ *
+ * Note Each toy can be purchased only once.
+ *
+ * Example
+ *
+ *
+ * The budget is  units of currency. He can buy items that cost  for , or  for  units. The maximum is  items.
+ *
+ * Function Description
+ *
+ * Complete the function maximumToys in the editor below.
+ *
+ * maximumToys has the following parameter(s):
+ *
+ * int prices[n]: the toy prices
+ * int k: Mark's budget
+ * Returns
+ *
+ * int: the maximum number of toys
+ */
+class SortingMarkToysSolution {
+    /**
+     *  Complete the 'maximumToys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY prices
+     *  2. INTEGER k
+     */
+    fun maximumToys(
+        prices: Array<Int>,
+        k: Int)
+    : Int {
+        var result = 0
+
+        return result
+    }
+}
