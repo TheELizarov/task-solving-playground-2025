f9487861e99b8a515d16938ca9d5db27fec92e7b
TheElizarov <elizarov1988@gmail.com>
2025-03-19 20:08:26 +0400

    Task LongestPalindromicSubstringSolution.kt: Добавлен файл для новой задачи

---
+++ src/main/kotlin/leetcode/LongestPalindromicSubstringSolution.kt
@@ -0,0 +1,28 @@
+package ru.theelizarov.leetcode
+
+/**
+ * Task https://leetcode.com/problems/longest-palindromic-substring/
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ *
+ * Example 1:
+ *
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * Example 2:
+ *
+ * Input: s = "cbbd"
+ * Output: "bb"
+ */
+class LongestPalindromicSubstringSolution {
+    fun longestPalindrome(
+        s: String)
+    : String {
+        var result = ""
+
+
+
+        return result
+    }
+}
